4e5afffd3fe95b139b481a3b28b0f3270087bd5d
jeyi95 <152843439+jeyi95@users.noreply.github.com>
2023-12-04 17:06:57 +0900

    Update spine_export.sh

--- spine_export.sh
+++ spine_export.sh
@@ -9,10 +9,10 @@ fi
 # Configuration variables
 # The easiest way to configure these, without breaking future script updates
 # would be to create a new script where these variables are properly setup
-GRAPHICS_PATH=${GRAPHICS_PATH:-"C:\Users\Bigglz\Desktop\spine_export_test\aaaa.spine"}
-EXPORT_PATH=${EXPORT_PATH:-"C:\Users\Bigglz\Desktop\spine_export_test"}
+GRAPHICS_PATH=${GRAPHICS_PATH:-"C:\Users\Bigglz\Desktop\spine_export_test"}
+EXPORT_PATH=${EXPORT_PATH:-"C:\Users\Bigglz\Desktop\spine_export_test\test"}
 SPINE_VERSION=${SPINE_VERSION:-"3.8.99"}
-EXPORT_SETTINGS=${EXPORT_SETTINGS:-"C:\Users\Bigglz\Desktop\aaaa.export.json"}
+EXPORT_SETTINGS=${EXPORT_SETTINGS:-"${GRAPHICS_PATH}/aaaa.export.json"}
 SPINE_EXE=${SPINE_EXE:-""}
 
 # try to auto detect spine executable path
